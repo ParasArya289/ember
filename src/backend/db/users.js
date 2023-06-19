2d90faddcc045e601bbc8023d9c384961c768a79
Paras Arya <parasarya289@gmail.com>
2023-06-20 00:10:51 +0530

    setup

---
+++ src/backend/db/users.js
@@ -0,0 +1,18 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Paras",
+    lastName: "Arya",
+    username: "parasarya",
+    password: "parasarya123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
