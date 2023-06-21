8e960912f0998ec480c1ddbe65573127ea214df4
Paras Arya <parasarya289@gmail.com>
2023-06-22 00:07:28 +0530

    setup

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -8,10 +8,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Paras",
-    lastName: "Arya",
-    username: "parasarya",
-    password: "parasarya123",
+    firstName: "Adarsh",
+    lastName: "Balika",
+    username: "adarshbalika",
+    password: "adarshBalika123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
