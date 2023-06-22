da7d593a0269d7328509d4ede2bc5563e98dd08f
Paras Arya <parasarya289@gmail.com>
2023-06-22 23:16:00 +0530

    style: background svg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Mockman from "mockman-js";
+import { FaEmber } from "react-icons/fa";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Mockman />
+      <FaEmber style={{ color: "white", fontSize: "5rem" }} />
+      {/* <Mockman /> */}
+      <Routes>
+        <Route path="/mockman" element={<Mockman />} />
+        <Route path="/auth"/>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {makeServer} from "./server";
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { makeServer } from "./server";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 makeServer();
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
