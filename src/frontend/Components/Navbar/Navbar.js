583ddd9f77d12754754c0fc0effe73fb4c2824de
Paras Arya <parasarya289@gmail.com>
2023-06-24 16:57:08 +0530

    style: search bar resposiveness and layout

--- src/frontend/Components/Navbar/Navbar.js
+++ src/frontend/Components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
+import { Searchbar } from "../Searchbar/Searchbar";
 import "./Navbar.css";
 export const Navbar = ({ title }) => {
   return (
     <nav className="feed-nav">
       <p>{title}</p>
+      <div className="feed-nav-search">
+        <Searchbar />
+      </div>
     </nav>
   );
 };
