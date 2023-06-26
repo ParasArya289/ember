a2252eaa3886a0a555f5a5ec5427fd2a4bb52ce8
Paras Arya <parasarya289@gmail.com>
2023-06-27 02:06:08 +0530

    feat: redirect enabled from auth to home

--- src/frontend/Pages/Auth/auth.js
+++ src/frontend/Pages/Auth/auth.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Login } from "./Login/Login";
 import { Signup } from "./Signup/Signup";
 import { motion } from "framer-motion";
@@ -9,47 +9,49 @@ import { useAuth } from "../../Context/authContext";
 
 export const Auth = () => {
   const [loginUi, setLoginUi] = useState("login");
-  //   const { token } = useContext(authContext);
-  const {token} = useAuth()
+  const { token } = useAuth();
   const [ref, { height }] = useMeasure();
   const navigate = useNavigate();
-  const location = useLocation();
 
-    useEffect(() => {
-      // if (token) {
-      //   const navi =
-      //     (location?.state?.from === "/user" && "/products") ||
-      //     (location?.state?.from ?? "/products");
-      //   navigate(navi);
-      // }
-      // navigate("/")
-    }, [token]);
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
   return (
     <div className="auth">
-
-
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: 1, y: 0, height: height + 70 }}
-      transition={{ duration: 0.4, delay: 0 }}
-      className="auth-container"
-    >
-      <header className="auth-header">
-        <button
-          style={{ background: loginUi !== "login" ? "rgba(38, 38, 38, 0.1)" : "rgba(64, 64, 64, 0.2)" }}
-          onClick={() => setLoginUi("login")}
-        >
-          LogIn
-        </button>
-        <button
-          style={{ background: loginUi !== "signup" ? "rgba(38, 38, 38, 0.1)" : "rgba(64, 64, 64, 0.2)" }}
-          onClick={() => setLoginUi("signup")}
-        >
-          SignUp
-        </button>
-      </header>
-      <div ref={ref}>{loginUi === "login" ? <Login /> : <Signup />}</div>
-    </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0, height: height + 70 }}
+        transition={{ duration: 0.4, delay: 0 }}
+        className="auth-container"
+      >
+        <header className="auth-header">
+          <button
+            style={{
+              background:
+                loginUi !== "login"
+                  ? "rgba(38, 38, 38, 0.1)"
+                  : "rgba(64, 64, 64, 0.2)",
+            }}
+            onClick={() => setLoginUi("login")}
+          >
+            LogIn
+          </button>
+          <button
+            style={{
+              background:
+                loginUi !== "signup"
+                  ? "rgba(38, 38, 38, 0.1)"
+                  : "rgba(64, 64, 64, 0.2)",
+            }}
+            onClick={() => setLoginUi("signup")}
+          >
+            SignUp
+          </button>
+        </header>
+        <div ref={ref}>{loginUi === "login" ? <Login /> : <Signup />}</div>
+      </motion.div>
     </div>
   );
 };
