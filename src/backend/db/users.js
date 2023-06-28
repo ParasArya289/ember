036123138645b6d1bcf4193de76e274ec2942357
Paras Arya <parasarya289@gmail.com>
2023-06-28 14:39:08 +0530

    update: hoverCard supports real user data

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -10,6 +10,8 @@ export const users = [
     _id: uuid(),
     avatar:
       "https://pbs.twimg.com/profile_images/1605458814068674563/V2sT6f64_400x400.jpg",
+    bio:"Tech-loving IT student | Passionate web developer | AWS Certified | Embracing innovation",
+    link:"https://parasarya.netlify.app/",
     firstName: "Paras",
     lastName: "Arya",
     username: "parasarya",
--- src/frontend/Components/ProfileHoverCard/ProfileHoverCard.js
+++ src/frontend/Components/ProfileHoverCard/ProfileHoverCard.js
@@ -1,34 +1,43 @@
 import "./ProfileHoverCard.css";
 // import React from 'react';
-import * as HoverCard from '@radix-ui/react-hover-card';
+import * as HoverCard from "@radix-ui/react-hover-card";
 
-export const ProfileHoverCard = ({children,user}) => (
+export const ProfileHoverCard = ({ children, user }) => (
   <HoverCard.Root>
     <HoverCard.Trigger className="HoverCardTrigger">
       {children}
     </HoverCard.Trigger>
     <HoverCard.Portal>
       <HoverCard.Content className="HoverCardContent" sideOffset={5}>
-        <div style={{ display: 'flex', flexDirection: 'column', gap: 7 }}>
-          <img
-            className="Image large"
-            src={user?.avatar}
-          />
-          <div style={{ display: 'flex', flexDirection: 'column', gap: 15 }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: 7 }}>
+          <img className="Image large" src={user?.avatar} />
+          <div style={{ display: "flex", flexDirection: "column", gap: 15 }}>
             <div>
-              <div className="Text bold">{user?.firstName} {user?.lastName}</div>
+              <div className="Text bold">
+                {user?.firstName} {user?.lastName}
+              </div>
               <div className="Text faded">@{user?.username}</div>
             </div>
             <div className="Text">
-              Components, icons, colors, and templates for building high-quality, accessible UI.
-              Free and open-source.
-            </div>
-            <div style={{ display: 'flex', gap: 15 }}>
-              <div style={{ display: 'flex', gap: 5 }}>
-                <div className="Text bold">0</div> <div className="Text faded">Following</div>
+              {user?.bio ?? ""}
+              <div>
+                <a href={user?.link} target="_blank">
+                  {user?.link}
+                </a>
               </div>
-              <div style={{ display: 'flex', gap: 5 }}>
-                <div className="Text bold">2,900</div> <div className="Text faded">Followers</div>
+            </div>
+            <div style={{ display: "flex", gap: 15 }}>
+              <div style={{ display: "flex", gap: 5 }}>
+                <div className="Text bold">
+                  {user?.following.toString() || "0"}
+                </div>{" "}
+                <div className="Text faded">Following</div>
+              </div>
+              <div style={{ display: "flex", gap: 5 }}>
+                <div className="Text bold">
+                  {user?.followers.toString() || "0"}
+                </div>{" "}
+                <div className="Text faded">Followers</div>
               </div>
             </div>
           </div>
