b95a01491df9578bcb1117943c273b36fa72b153
Paras Arya <parasarya289@gmail.com>
2023-06-30 01:01:31 +0530

    style: ui of dialogbox

--- src/frontend/Components/PostDialogBox/PostDialogBox.js
+++ src/frontend/Components/PostDialogBox/PostDialogBox.js
@@ -2,33 +2,45 @@ import React from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { RxCross2 } from "react-icons/rx";
 import "./PostDialogBox.css";
+import { useAuth } from "../../Context/authContext";
 
-const PostDialogBox = ({ children }) => (
-  <Dialog.Root>
-    <Dialog.Trigger className="DialogTrigger">{children}</Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="DialogOverlay" />
-      <Dialog.Content className="DialogContent">
-        <Dialog.Title className="DialogTitle">Post</Dialog.Title>
-        <Dialog.Description className="DialogDescription">
-          Make changes to your profile here. Click save when you're done.
-        </Dialog.Description>
-       
-        <div
-          style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}
-        >
+const PostDialogBox = ({ children }) => {
+  const {user} = useAuth();
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="DialogTrigger">{children}</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="DialogOverlay" />
+        <Dialog.Content className="DialogContent">
+          <Dialog.Title className="DialogTitle">
+            <div className="DialogUser">
+              <img src={user?.avatar} height="40"/>
+            </div>
+          </Dialog.Title>
+          <Dialog.Description className="DialogDescription">
+            <textarea placeholder="Whats happening?!"/>
+          </Dialog.Description>
+
+          <div
+            style={{
+              display: "flex",
+              marginTop: 25,
+              justifyContent: "flex-end",
+            }}
+          >
+            <Dialog.Close asChild>
+              <button className="Button green">Post</button>
+            </Dialog.Close>
+          </div>
           <Dialog.Close asChild>
-            <button className="Button green">Post</button>
+            <button className="IconButton" aria-label="Close">
+              <RxCross2 />
+            </button>
           </Dialog.Close>
-        </div>
-        <Dialog.Close asChild>
-          <button className="IconButton" aria-label="Close">
-            <RxCross2 />
-          </button>
-        </Dialog.Close>
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-);
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
 
 export default PostDialogBox;
