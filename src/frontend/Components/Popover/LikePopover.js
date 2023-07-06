d7bb0e885fa5c5294c26c9357a01d5f4862ca20b
Paras Arya <parasarya289@gmail.com>
2023-07-06 14:40:16 +0530

    fix: likepopover close navigating to user profile

--- src/frontend/Components/Popover/LikePopover.js
+++ src/frontend/Components/Popover/LikePopover.js
@@ -1,20 +1,28 @@
-import * as React from "react";
 import * as Popover from "@radix-ui/react-popover";
 import "./Popover.css";
 import { useData } from "../../Context/dataContext";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const LikePopover = ({ children, likedBy }) => {
+  const [open, setOpen] = useState(false);
   const {
     dataState: { users },
   } = useData();
   const navigate = useNavigate();
+
+  const navigateToUserProfile = (username) => {
+    if (username) {
+      navigate("/profile/" + username);
+      setOpen(false);
+    }
+  };
   const listLikedUsers = users?.filter(({ username }) =>
     likedBy?.some(({ username: name }) => name === username)
   );
   return (
     <div className="Popover">
-      <Popover.Root>
+      <Popover.Root open={open} onOpenChange={setOpen}>
         <Popover.Trigger className="PopoverTrigger">
           {" "}
           {children}
@@ -25,7 +33,7 @@ export const LikePopover = ({ children, likedBy }) => {
               <div
                 key={user?._id}
                 style={{ marginBlock: "10px", fontSize: "small" }}
-                onClick={() => navigate("/profile/" + user?.username)}
+                onClick={() => navigateToUserProfile(user?.username)}
               >
                 <img
                   src={user?.avatar}
